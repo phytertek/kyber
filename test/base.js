5a1d7de90f363730974eb783fd3b34da9bcc4079
Phytertek <ryan.phytertek@gmail.com>
2017-06-11 15:29:44 -0400

    Create base test

---
+++ test/base.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+// test utilities
+import {request, should} from './'
+import {app as appConfig} from '../config'
+
+describe('Base Functionality', () => {
+  describe('GET', () => {
+    describe('/#Non Existing Url', () => {
+      it('should return a status 404', (done) => {
+        request(appConfig.hostPort).get('/abadurlthatwontwork').end((err, res) => {
+          err.should.not.be.null
+          done()
+        })
+      });
+    });
+  });
+});
+
